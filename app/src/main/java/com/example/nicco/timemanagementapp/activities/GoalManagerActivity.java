07d16cbe47639385365679c7f6640a512fb2d1f7
Niccolo Pucci <npucci@sfu.ca>
2017-11-02 21:39:53 -0700

    Add create new goal Activity

--- app/src/main/java/com/example/nicco/timemanagementapp/activities/GoalManagerActivity.java
+++ app/src/main/java/com/example/nicco/timemanagementapp/activities/GoalManagerActivity.java
@@ -1,17 +1,35 @@
 package com.example.nicco.timemanagementapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.nicco.timemanagementapp.R;
 
 public class GoalManagerActivity extends AppCompatActivity
 {
+    private Button createNewGoalbutton;
 
     @Override
     protected void onCreate ( Bundle savedInstanceState )
     {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_goal_manager );
+
+        createNewGoalbutton = ( Button ) findViewById ( R.id.createNewGoalbutton );
+        createNewGoalbutton.setOnClickListener ( new View.OnClickListener ()
+        {
+            @Override
+            public void onClick ( View v )
+            {
+                startActivity ( new Intent(
+                        GoalManagerActivity.this,
+                        CreateNewGoalActivity.class
+                ) );
+            }
+        } );
+
     }
 }
